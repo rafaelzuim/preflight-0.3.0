429db67dd7c23b6a4d7d46681881fbbf945fa00b
Evandro Morini <evandro.morini@gmail.com>
2014-11-03 10:54:15 -0200

    Classe Color Validator

---
+++ lib/preflight/rules/color_validator.rb
@@ -0,0 +1,68 @@
+# coding: utf-8
+
+require 'yaml'
+require 'matrix'
+
+module Preflight
+  module Rules
+
+    # * CLASSE REESCRITA PARA VALIDAÇÃO ESPECÍFICA DAS CORES
+    #
+    # *Preto carregado (Preto K, misturado com todas as demais cores C, M e Y)
+    # *Azul carregado (Azul com Cyan e Magenta igual ou superior a 99%)
+    #
+    
+    class ColorValidator
+
+      attr_reader :issues
+
+      # we're about to start a new page, reset state
+      #
+      def page=(page)
+        @issues = []
+        @page    = page
+        @objects = page.objects
+      end
+
+      def set_cmyk_color_for_nonstroking(c, m, y, k)
+        check_ink(c, m, y, k)
+      end
+
+      def set_cmyk_color_for_stroking(c, m, y, k)
+        check_ink(c, m, y, k)
+      end
+
+      private
+
+      def check_ink(c, m, y, k)
+        ink = (c + m + y + k) * 100.0
+        cyan = c * 100.0
+        magenta = m * 100.0
+        yellow = y * 100.0
+        black = k * 100.0
+        average = ink / 4.0
+
+        #Valida Preto carregado
+        if 
+          (black >= 85.0 && average >= 70.0) 
+            @issues << Issue.new("A cor preta está carregada", self, 
+              :page    => @page.number,
+              :cyan    => c,
+              :magenta => m,
+              :yellow  => y,
+              :k      => k)
+        #Valida Azul carregado
+        elsif
+        (cyan >= 99.0 && magenta >= 99.0 && average <= 70.0) 
+            @issues << Issue.new("A cor azul está carregada", self, 
+              :page    => @page.number,
+              :cyan    => c,
+              :magenta => m,
+              :yellow  => y,
+              :k      => k)
+        end
+      end
+    end
+
+  end
+end
